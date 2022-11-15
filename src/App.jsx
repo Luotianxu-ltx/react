f97089e3611b34516b7728f82b91b3f9d2c871c0
luotianxu <1265062404@qq.com>
2022-11-15 21:57:54 +0800

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import Count from './containers/Count'
-import store from './redux/store.js'
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Count store={store}></Count>
+                <Count></Count>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // 引入react核心库
 import React from 'react'
-
 import { createRoot } from 'react-dom/client'
 // 引入App组件
 import App from './App'
 import store from './redux/store'
+import { Provider } from 'react-redux'
 
 const root = createRoot(document.getElementById('root'))
-root.render(<App></App>)
-
-store.subscribe(() => {
-    root.render(<App></App>)
-})
+root.render(
+    <Provider store={store}>
+        <App></App>
+    </Provider>
+)
